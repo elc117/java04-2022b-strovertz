e288202890c5eb5064b895536fdb3950a0e35b8b
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-10-04 11:48:03 -0300

    atualizando

---
+++ Laboratory.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public class Laboratory {
+  private String name;
+  private ArrayList<Student> students;
+  private ArrayList<Professor> professors;
+
+  public Laboratory() {
+  }
+
+  public Laboratory(String name) {
+    this.name = name;
+    this.students = new ArrayList<Student>();
+    this.professors = new ArrayList<Professor>();
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void addMember(Student s) {
+    this.students.add(s);
+  }
+
+  public void addMember(Professor p) {
+    this.professors.add(p);
+  }
+  
+  public ArrayList<String> getContactInfos() {
+    // COMPLETE-ME
+    // Construa um ArrayList<String> contendo informações de contato (ContactInfo)
+    // de cada um dos estudantes e professores
+    ArrayList<String> contacts = new ArrayList<>();
+
+    contacts.add("99555");
+    
+    return contacts;
+  }
+
+  public boolean userExists(String userId) {
+    // COMPLETE-ME
+    // Verifique se existe o userId na lista de estudantes ou de professores
+    for (int i = 0; i < professors.size(); i++) {
+        if(userId == students.get(i).getUserId()){
+            return false;
+        }
+      }
+    return true;
+  }
+
+  public int countMembers() {
+    // COMPLETE-ME
+    // Retorne o total de membros do laboratório (estudantes e professores)
+    int totalsize = professors.size() + students.size();
+    return totalsize;
+  }
+
+
+}
